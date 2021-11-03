72cb1bfe5b19a46bb7e686d9bf0b8e3eb80e0588
Ian Wu <ianwu1099@gmail.com>
2021-11-02 21:52:57 -0400

    image background size mathcing implimented

--- replace_bg.py
+++ replace_bg.py
@@ -1,8 +1,31 @@
 import cv2
 import numpy as np
 
-image_path = "liz.jpg"
-map_path = "liz-map.jpg"
+
+def bg_size_match(dim, bg_img):
+    bg_dim = bg_img.shape
+
+    if bg_dim[0] == dim[0] and bg_dim[1] == dim[1]:
+        return bg_img
+    elif bg_dim[0] > dim[0] and bg_dim[1] > dim[1]:
+        offset_x = int((bg_dim[1] - dim[1]) / 2)
+        offset_y = int((bg_dim[0] - dim[0]) / 2)
+        return bg_img[offset_y:offset_y+dim[0], offset_x:offset_x+dim[1]]
+    elif bg_dim[0] < dim[0] and (bg_dim[1] > dim[1] or bg_dim[1] == dim[1]):
+        bg_img = cv2.resize(bg_img, (bg_dim[1], dim[0]))
+        offset_x = int((bg_dim[1] - dim[1]) / 2)
+        return bg_img[0:dim[0], offset_x:offset_x+dim[1]]
+    elif (bg_dim[0] > dim[0] or bg_dim[0] == dim[0]) and bg_dim[1] < dim[1]:
+        bg_img = cv2.resize(bg_img, (dim[1], bg_dim[0]))
+        offset_y = int((bg_dim[0] - dim[0]) / 2)
+        return bg_img[offset_y:offset_y+dim[0], 0:dim[1]]
+    else:
+        return cv2.resize(bg_img, (dim[1], dim[0]))
+
+
+def map_normalization(map):
+
+    print()
 
 
 def combine():
@@ -19,4 +42,3 @@ def combine():
     cv2.imshow("removed-bg", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    
